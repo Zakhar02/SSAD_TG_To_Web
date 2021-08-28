66dd23b18f3acff2801b2d71fd98bf9a47259dee
Yagudin Zakhar Ruslanovich <zyagudin@ozon.ru>
2021-08-28 21:21:51 +0300

    sample project

---
+++ Tg_To_Web/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for Tg_To_Web project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Tg_To_Web.settings')
+
+application = get_asgi_application()
---
+++ Tg_To_Web/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for Tg_To_Web project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Tg_To_Web.settings')
+
+application = get_wsgi_application()
