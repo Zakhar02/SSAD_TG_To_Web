906d12ebdf41f25fed967d5acff51dfbe10c37a7
Yagudin Zakhar Ruslanovich <zyagudin@ozon.ru>
2021-09-08 13:25:06 +0300

    Changed DB, added docker-compose

--- Tg_To_Web/settings.py
+++ Tg_To_Web/settings.py
@@ -76,9 +76,13 @@ WSGI_APPLICATION = 'Tg_To_Web.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": "tgtoweb",
+        "USER": "postgres",
+        "PASSWORD": "postgres",
+        "HOST": "localhost",
+        "PORT": 5432,
     }
 }
 
